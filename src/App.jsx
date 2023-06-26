e71cd31e046374ff86977358b94b62ffe07fd738
dev-rahim <rahimuddin5915@gmail.com>
2023-06-26 13:36:49 +0600

    massenger app created and added tailwindcss and daysi UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-8xl first-letter:font-bold underline">
+        Hello world!
+      </h1>
+
+      <button className="btn btn-active btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default App;
